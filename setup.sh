312a02e209669c082d82d5867b05c6d551cdf9a1
sabonis <sabonis.tw@gmail.com>
2019-08-03 13:50:42 +0800

    Fix vim input is not from terminal error

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ ln -s $(pwd)/.vimrc ~/.vimrc
 # see https://github.com/k-takata/minpac
 git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
 # see https://superuser.com/questions/451863/how-to-run-vim-command-from-the-shell
-vim +'call minpac#update()'
+vim - +'call minpac#update()'
 
