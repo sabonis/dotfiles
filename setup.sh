535a00ee533d28040f8e06978b15220e4cc25c4c
sabonis <sabonis.tw@gmail.com>
2019-08-04 07:11:23 +0800

    Add missing bootstrap codes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 
+MAIN_PATH=~/dotfiles
+
+git clone git@github.com:sabonis/dotfiles.git $MAIN_PATH
+
 mkdir -p ~/.vim/undodir
-ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $MAIN_PATH/.vimrc ~/.vimrc
 
 # Install minpac
 # see https://github.com/k-takata/minpac
