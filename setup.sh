827675265c13ea9d3a7db811a2a5ff48f3c545c4
sabonis <sabonis.tw@gmail.com>
2019-12-27 13:57:18 +0800

    :sparkles: Add tmux config

--- setup.sh
+++ setup.sh
@@ -1,18 +1,24 @@
 #! /usr/bin/env bash
 
 echo Installing...
-MAIN_PATH=~/dotfiles
+MAIN_PATH=$(pwd $(dirname $0))
+echo "MAIN_PATH = $MAIN_PATH"
 
-git clone git@github.com:sabonis/dotfiles.git $MAIN_PATH
+if [ ! -d $MAIN_PATH ]; then
+    git clone git@github.com:sabonis/dotfiles.git $MAIN_PATH
+fi
 
 mkdir -p ~/.vim/undodir
-ln -s $MAIN_PATH/.vimrc ~/.vimrc
+ln -sf $MAIN_PATH/.vimrc ~/.vimrc
+
+# Link tmux
+ln -sf "$MAIN_PATH/.tmux.conf.local" ~/.tmux.conf.local
 
 # Install minpac
 # see https://github.com/k-takata/minpac
-git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
+# git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
 # see https://superuser.com/questions/451863/how-to-run-vim-command-from-the-shell
-vim +"call minpac#update('', {'do': 'q'})"
+# vim +"call minpac#update('', {'do': 'q'})"
 
 echo Installed 🍺
 
